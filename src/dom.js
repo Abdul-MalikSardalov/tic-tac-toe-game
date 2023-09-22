9c38b3620ba7d64d0f1feb741cffc0b7121196b6
Abdul-MalikSardalov <msar9565@gmail.com>
2023-09-22 19:10:50 +0200

    ready project without separation of concerns

--- src/dom.js
+++ src/dom.js
@@ -1,3 +1,7 @@
-const dom = {};
+const dom = {
+    message: document.querySelector('.message'),
+    goes: 'circle',
+    restartBtn: document.querySelector('.restartBtn'),
+};
 
 export default dom;
--- src/init.js
+++ src/init.js
@@ -1,5 +1,80 @@
 // import your listeners(events)
 
-import sumListener from './events/sumListener.js';
+import data from './data.js';
+import dom from './dom.js';
 
-sumListener();
+const checkScore = () => {
+    let haveWinner = false;
+    const allSquares = document.querySelectorAll('.square');
+
+    data.winningCombos.forEach((combo) => {
+        const circleWin = combo.every((index) =>
+            allSquares[index].firstChild?.classList.contains('circle'),
+        );
+
+        if (circleWin) {
+            let haveWinner = true;
+
+            allSquares.forEach(
+                (square) => square.removeEventListener('click', addGo),
+                (dom.message.innerHTML = 'Circles win'),
+            );
+        }
+    });
+
+    data.winningCombos.forEach((combo) => {
+        const crossesWin = combo.every((index) =>
+            allSquares[index].firstChild?.classList.contains('cross'),
+        );
+
+        if (crossesWin) {
+            let haveWinner = true;
+            allSquares.forEach(
+                (square) => square.removeEventListener('click', addGo),
+                (dom.message.innerHTML = 'Crosses win'),
+            );
+        }
+    });
+
+    if (
+        !haveWinner &&
+        Array.from(allSquares).every((square) => square.firstChild)
+    ) {
+        dom.message.innerHTML = "It's a draw!";
+    }
+};
+
+const addGo = (e) => {
+    dom.message.innerHTML = '';
+    const goDisplay = document.createElement('div');
+    goDisplay.classList.add(dom.goes);
+
+    dom.goes = dom.goes === 'circle' ? 'cross' : 'circle';
+    e.target.append(goDisplay);
+    e.target.removeEventListener('click', addGo);
+
+    checkScore();
+};
+
+const restartGame = () => {
+    dom.restartBtn.addEventListener('click', () => {
+        const gameBoard = document.querySelector('.game-board');
+        gameBoard.innerHTML = '';
+
+        createCells();
+    });
+};
+const createCells = () => {
+    dom.message.innerHTML = 'Circles goes first';
+    data.cells.forEach((cell, index) => {
+        const cellElement = document.createElement('div');
+        cellElement.classList.add('square');
+        cellElement.id = index;
+        cellElement.addEventListener('click', addGo);
+        document.querySelector('.game-board').append(cellElement);
+    });
+
+    restartGame();
+};
+
+createCells();
